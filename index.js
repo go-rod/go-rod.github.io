41b121c2e5e8650c2d9ad47f4009528d99462c7b
Yad Smood <y.s.inside@gmail.com>
2023-06-10 13:19:29 +0800

    add spell check

--- index.js
+++ index.js
@@ -68,13 +68,5 @@
         gtag('event', 'conversion', { path: vm.route.path })
       }, 30*1000);
     })
-  
-    hook.mounted(function() {
-      ;(function (c, l, a, r, i, t, y) {
-        c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
-        t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
-        y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
-      })(window, document, "clarity", "script", "b4efllwpue");
-    })
   }
 })()
